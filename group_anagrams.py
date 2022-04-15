5361d974e1822b66314a8dde1e1b7ac21d1409b0
aux-Issa <issa1116184028@gmail.com>
2022-04-15 13:13:20 +0900

    group_anagrams

---
+++ group_anagrams.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/group-anagrams/
+# solution: https://p0ny.hatenablog.com/entry/2020/03/15/213608
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = {}
+        ans = []
+        for word in strs:
+            sorted_chars = "".join(sorted(word))
+            if sorted_chars in dic:
+                dic[sorted_chars].append(word)
+            else:
+                dic[sorted_chars] = [word]
+        for s in dic:
+            ans.append(dic[s])
+        return ans
