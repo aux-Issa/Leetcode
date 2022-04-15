45d040be0398f06b3d5c201d581d67b57e788c12
aux-Issa <issa1116184028@gmail.com>
2022-04-15 15:01:05 +0900

    unique-email-addresses.py

--- unique-email-addresses.py
+++ unique-email-addresses.py
@@ -5,6 +5,7 @@ class Solution:
         emails_dic = {"local":[], "domain":[]}
         for email in emails:
             emails_list = email.split('@')
+            # localの.を削除し，+以降を除去
             emails_dic['local'].append(emails_list[0].replace('.', '').split('+')[0])
             emails_dic['domain'].append(emails_list[1])
 
@@ -13,6 +14,8 @@ class Solution:
         for l in emails_dic["local"]:
             ans.append(l.join(emails_dic["domain"][i]))
             i += 1
+        # 集合型にして重複を削除
         ans = set(ans)
+        # リスト型に戻す
         ans = list(ans)
         return len(ans)
